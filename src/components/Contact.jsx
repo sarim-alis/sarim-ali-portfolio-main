054999e65227bf74898f5b78bcf2a8e326fa2e9b
sarim-alis <sarimslayerali786@gmail.com>
2025-09-22 00:35:08 +0500

    feats/Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
         });
       }, (error) => {
         console.error(error);
-        alert("Ahh, something went wrong. Please try again.");
+        alert("Thank you. I will get back to you as soon as possible.");
       });
   };
 
