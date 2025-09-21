451187dfaaf7683c630e96e5147192315be38560
sarim-alis <sarimslayerali786@gmail.com>
2025-09-21 23:45:43 +0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import live from "./live.png";
+import chatapp from "./chatapp.jpg";
+
+import javascript from "./tech/javascript.png";
+import typescript from "./tech/typescript.png";
+import mongodb from "./tech/mongodb.png";
+import expressjs from "./tech/expressjs.png";
+import reactjs from "./tech/reactjs.png";
+import nodejs from "./tech/nodejs.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import corvit from "./company/corvit.png";
+import code from "./company/code.png";
+import digital from "./company/digital.png";
+import tecnsol from "./company/tecnsol.png";
+import lab from "./company/lab.jfif";
+import fa from "./company/fa.jfif";
+import codedb from "./company/codedb.jfif";
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  javascript,
+  typescript,
+  mongodb,
+  expressjs,
+  reactjs,
+  nodejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  corvit,
+  code,
+  digital,
+  tecnsol,
+  live,
+  chatapp,
+  lab,
+  fa,
+  codedb,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,223 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  reactjs,
+  expressjs,
+  nodejs,
+  mongodb,
+  jobit,
+  tripguide,
+  corvit,
+  code,
+  digital,
+  tecnsol,
+  chatapp,
+  lab,
+  codedb,
+  fa
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Express JS",
+    icon: expressjs,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+];
+
+const experiences = [
+  {
+    title: "React Developer",
+    company_name: "Code Debuggers",
+    icon: codedb,
+    iconBg: "white",
+    date: "May 2023 - June 2024 ",
+    points: [
+      "Developed 10+ dynamic projects utilizing React.",
+      "Developed test suites which reduced deployment errors by 40 percent.",
+      "Used Redux leading to 25 percent improvement in application performance.",
+      "Implemented responsive design resulting in 30% increase in user retention.",
+    ],
+  },
+  {
+    title: "Mern Stack Developer",
+    company_name: "Fa Tech & Solutions",
+    icon: fa,
+    iconBg: "white",
+    date: "July 2024 - Sep 2024",
+    points: [
+      "Successfully completed over 3 full stack projects.",
+      "Completed deployment of applications leading to 30% increase in efficiency.",
+      "Integrated MongoDB resulting in 15 percent improvement in application performance.",
+      "Maintained robust backend APIs, cutting post-deployment bugs by 30%.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Andropplelab",
+    icon: lab,
+    iconBg: "white",
+    date: "Oct 2024 - Present",
+    points: [
+      "Delivered more than 5 full stack projects.",
+      "Optimized performance by reducing load times by up to 20%.",
+      "Implemented responsive design resulting in 30 percent increase in user engagement.",
+      "Streamlined PostgreSQL with prisma increasing backend performance by 25%.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Sarim proved me wrong.",
+    names: "Maryam Ahmad",
+    designation: "Instructor",
+    company: "Corvit",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Sarim does.",
+    names: "Muhammad Awais",
+    designation: "CEO",
+    company: "Code Debuggers",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Sarim optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    names: "Iqra Khalid",
+    designation: "Trainer",
+    company: "Tecnsol",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    names: "Chat App",
+    description:
+      " Developed for real-time messaging. Implements user authentication with JWT. Socket.io is employed to enable live communication between clients and the server.",
+    tags: [
+      {
+        names: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        names: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        names: "express",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: chatapp,
+    live_link: "https://sarim-chat-app.onrender.com",
+    source_code_link: "https://github.com/sarimuchihaa/sarim-chat-app",
+  },
+  {
+    names: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        names: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        names: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        names: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    live_link: "https://sarim-chat-app.onrender.com",
+    source_code_link: "https://github.com/",
+  },
+  {
+    names: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        names: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        names: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        names: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    live_link: "https://sarim-chat-app.onrender.com",
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
